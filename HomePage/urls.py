439a8d201135435f69f9b8857b2d7533eef0d5d4
bb <bbuzz318@icloud.com>
2017-05-29 17:03:46 -0400

    Added options for developing from scratch
    Downloaded actual bootstrap and jquery
    Download a garage bootstrap template

--- HomePage/urls.py
+++ HomePage/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import *
 from . import views
 
 urlpatterns = [url(r'^$', views.HomePage, name='HomePage'),
+               url(r'^_nav', views._nav, name='BaseOld'),
                url(r'^Base', views.BasePage, name='BasePage'),
-               url(r'^ODU', views.ODUHead, name='BasePage'),
                url(r'^adams', views.Adams, name='AdamsPage'),
 ]
--- HomePage/views.py
+++ HomePage/views.py
@@ -6,8 +6,11 @@ def HomePage(request):
     # template directory must be on path in settings.py
     return render(request, 'home.html')
 
+def _nav(request):
+    return render(request, '_nav.html')
+
 def BasePage(request):
-    return render(request, 'base.html')
+    return render(request, '__base.html')
 
 def ODUHead(request):
     return render(request, 'ODU_head.html')
