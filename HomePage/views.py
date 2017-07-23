e72afa29dc906f5d1d2d1c061fda297618821ebf
bb <bbuzz318@icloud.com>
2017-07-23 15:18:42 -0400

    Carousel made from loop

--- HomePage/views.py
+++ HomePage/views.py
@@ -1,11 +1,11 @@
 from django.http import Http404, HttpResponse, HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render
+from BH_LAB.settings import STATICFILES_DIRS
+import os
+import os.path as op
 
 def HomePage(request):
-    # return HttpResponse(render, 'index_dummy.html')
     # template directory must be on path in settings.py
-    return render(request, 'homepage.html')
-
-# not needed
-def Full(request):
-    return render(request, 'index.html')
+    carousel_images = os.listdir(op.join(STATICFILES_DIRS[0], 'Images', 'home'))
+    context = {'carousel_image_list' : carousel_images}
+    return render(request, 'homepage.html', context)
